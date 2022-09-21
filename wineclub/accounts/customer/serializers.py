fbab57f396a65783a71ad4a1b972cda354667852
Toan <crisriorock0@gmail.com>
2022-09-21 15:53:02 +0700

    update for customer account

--- wineclub/accounts/customer/serializers.py
+++ wineclub/accounts/customer/serializers.py
@@ -1,8 +1,14 @@
-from dataclasses import fields
+from pyexpat import model
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
+
+User = get_user_model()
+
+
 
 class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
+        model = User
         fields = [
             "email",
             "phone",
@@ -13,14 +19,29 @@ class ProfileSerializer(serializers.ModelSerializer):
             "stripe_account",
         ]
         read_only_fields = [
-            "points"
+            "points",
+            "stripe_account",
+            "email"
         ]
         
         
 class ChangePasswordSerializer(serializers.ModelSerializer):
+    old_password = serializers.CharField(required=True)
+    new_password = serializers.CharField(required=True)
+    confirm_password = serializers.CharField(required=True)
+    
     class Meta:
+        model = User
         fields = [
-           "password",
+           "old_password",
            "new_password",
            "confirm_password",
+        ]
+
+
+class ImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = [
+            "image"
         ]
