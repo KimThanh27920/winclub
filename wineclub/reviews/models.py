651845695752416dd7136410880e4d84dac0943b
Kim Thanh <thanh27920@gmail.com>
2022-09-21 16:43:13 +0700

    add review model

--- wineclub/reviews/models.py
+++ wineclub/reviews/models.py
@@ -1,3 +1,27 @@
+#django import
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+#app import
+from wines.models import Wine
+
+User = get_user_model()
+
+
+# Review model class
+class Review(models.Model):
+    content = models.CharField(max_length=255)
+    rating = models.IntegerField()
+    wine = models.ForeignKey(Wine, on_delete=models.CASCADE, related_name="wine_rating")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_rating")
+    created_at = models.DateTimeField()
+    # admin can be deleted
+    deleted_at = models.DateTimeField(null=True, blank=True, default=None)
+    deleted_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name="rating_deleted") 
+
+    def __str__(self) -> str:
+        return self.rating
+    
+
+    class Meta:
+        db_table = "reviews"
