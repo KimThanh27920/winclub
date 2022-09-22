22f10122f5ea4452268a2ea9e1ebdb1911afa1ae
Kim Thanh <thanh27920@gmail.com>
2022-09-22 17:08:46 +0700

    lock all branches related to Delivery

--- wineclub/addresses/customer/serializers.py
+++ wineclub/addresses/customer/serializers.py
@@ -1,42 +1,42 @@
-from dataclasses import field
-from rest_framework import serializers
-from ..models import Delivery, Address
+# from dataclasses import field
+# from rest_framework import serializers
+# from ..models import  Address
 
 
-class AddressSerializer(serializers.ModelSerializer):
+# class AddressSerializer(serializers.ModelSerializer):
 
-    class Meta:
-        model = Address
-        fields = [
-            "id",
-            "street",
-            "ward",
-            "district",
-            "city",
-            "country",
-        ]
+#     class Meta:
+#         model = Address
+#         fields = [
+#             "id",
+#             "street",
+#             "ward",
+#             "district",
+#             "city",
+#             "country",
+#         ]
 
 
-class DeliverySerializer(serializers.ModelSerializer):
-    address = AddressSerializer(read_only=True)
-    class Meta:
-        model = Delivery
-        fields = [
-            "id",
-            "full_name",
-            "phone",
-            "is_default",
-            "type",
-            "address",
-        ]
+# class DeliverySerializer(serializers.ModelSerializer):
+#     address = AddressSerializer(read_only=True)
+#     class Meta:
+#         model = Delivery
+#         fields = [
+#             "id",
+#             "full_name",
+#             "phone",
+#             "is_default",
+#             "type",
+#             "address",
+#         ]
 
-class CreateDeliverySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Delivery
-        fields = [
-            "id",
-            "full_name",
-            "phone",
-            "is_default",
-            "type",
-        ]
+# class CreateDeliverySerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Delivery
+#         fields = [
+#             "id",
+#             "full_name",
+#             "phone",
+#             "is_default",
+#             "type",
+#         ]
