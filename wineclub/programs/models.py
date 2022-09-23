9a07c9fc10c72b72e7c53cb120921959156f6380
Toan <crisriorock0@gmail.com>
2022-09-23 12:10:10 +0700

    update Model Reward Program

--- wineclub/programs/models.py
+++ wineclub/programs/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth import get_user_model
 
 #app import
 from coupons.models import Coupon
+from wines.models import Wine
 from bases.models import BasicLogModel
 
 User = get_user_model()
@@ -11,11 +12,13 @@ User = get_user_model()
 
 # Reward Program model class
 class RewardProgram(BasicLogModel):
-    name = models.CharField(max_length=255)
-    members = models.ManyToManyField(User, related_name="membership_list" )
-    require_sending = models.BooleanField(default=False)
-    coupons = models.ManyToManyField(Coupon, related_name="coupons_list")
-    descriptions= models.TextField()
+    name = models.CharField(max_length=255)    
+    require_sending = models.BooleanField(default=False)    
+    total_price_require = models.IntegerField(default=0)
+    coupon = models.ManyToManyField(Coupon, related_name="coupons_list")
+    wine = models.ManyToManyField(Wine, related_name="wine_list", null=True)
+    member = models.ManyToManyField(User, related_name="membership_list", null=True)
+    description = models.TextField()
     start = models.DateTimeField()
     end = models.DateTimeField()
     
