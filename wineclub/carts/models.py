4b926a741ab2a0b6d939655146588f2e649e3ac4
Toan <crisriorock0@gmail.com>
2022-09-23 11:33:48 +0700

    update model Cart

--- wineclub/carts/models.py
+++ wineclub/carts/models.py
@@ -11,14 +11,6 @@ Account = get_user_model()
 class Cart(models.Model):
     winery = models.ForeignKey(Winery, on_delete=models.CASCADE, related_name="winery_cart")
     account = models.ForeignKey(Account, on_delete=models.CASCADE, related_name="account_cart")
-
-    created_at = models.DateField(auto_now_add=True)
-    updated_at = models.DateField(auto_now=True)
-    deleted_at = models.DateField()
-
-    updated_by = models.ForeignKey(Account, on_delete=models.CASCADE, related_name="cart_updated")
-    deleted_by = models.ForeignKey(Account, on_delete=models.CASCADE, related_name="cart_deleted")
-
     class Meta:
         unique_together = (("winery", "account"),)
 
