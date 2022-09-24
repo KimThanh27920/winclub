9f544021a6f21221db0b0ac550d010166a0357ed
Toan <crisriorock0@gmail.com>
2022-09-24 11:52:13 +0700

    update requirements and account model set birthday null

--- wineclub/accounts/models.py
+++ wineclub/accounts/models.py
@@ -35,7 +35,7 @@ class Account(AbstractBaseUser):
     email = models.EmailField(max_length=255, unique=True)
     phone = models.CharField(max_length=255, unique=True)
     full_name = models.CharField(max_length=255, null=True)
-    birthday = models.DateField(max_length=255)
+    birthday = models.DateField(max_length=255, null=True)
     gender = models.BooleanField(default=True)
     points = models.IntegerField(default=0)
     image = models.ImageField(null=True, upload_to = "images/profile/")
