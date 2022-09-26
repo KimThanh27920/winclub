dc2a1a2582069699eb7a3232a2ac616477996637
huudangnguyen <nguyenhuudang0206@gmail.com>
2022-09-26 08:40:09 +0700

    Admin manage shippingunit

---
+++ wineclub/shipping/administrator/serializers.py
@@ -0,0 +1,21 @@
+from rest_framework import serializers
+from shipping.models import ShippingUnit
+
+
+class ShippingUnitSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = ShippingUnit
+        fields = [
+                "id",
+                "name", 
+                "fee", 
+                "type", 
+                "expected_date"
+                ]
+
+class UpdateStatusShippingUnitSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = ShippingUnit
+        fields = ["is_active"]
