2afdcb9e85d33482aebc8fb638b0afc17c59a800
tantrung3t <tantrung.dmc@gmail.com>
2022-09-28 16:37:15 +0700

    update feat register customer

--- wineclub/accounts/guest/serializers.py
+++ wineclub/accounts/guest/serializers.py
@@ -63,8 +63,6 @@ class RegisterSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         user = User.objects.create_user(**validated_data)  
         CouponOwner.objects.create(account=user)         #Toan cus
-              
-        user = User.objects.create_user(**validated_data)
         return user
 
 
