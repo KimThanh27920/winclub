fba25df44edc7b04f3a692622cabb8457bb94474
tantrung3t <tantrung.dmc@gmail.com>
2022-09-29 16:35:42 +0700

    update webhook catch event charge.success

--- wineclub/payments/views.py
+++ wineclub/payments/views.py
@@ -6,6 +6,9 @@ from bases.exception.exceptions import response_exception
 from bases.services.stripe.stripe import stripe_webhook
 from bases.services.stripe.stripe import stripe_transaction
 from bases.services.stripe.webhook import update_account
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 # Create your views here.
 
 from transactions.models import Transaction
@@ -36,24 +39,22 @@ def create_charge_success_transaction(event):
     """
     Create transactions
     """
-    # data = {
-    #     "timestamp": event.created,
-    #     "type": "charge",
-    #     "amount": event.data.object.amount,
-    #     "currency": event.data.object.currency,
-    #     "net": 0,
-    #     "fee": 0,
-    #     "unit": 100,
-    #     "description": "Charge for order ",
-    #     "sender": event.data.object.customer_email,
-    #     "receiver": "Platform",
-    # }
-    print(event)
+    data = {
+        "timestamp": event.created,
+        "type": "charge",
+        "amount": event.data.object.amount,
+        "currency": event.data.object.currency,
+        "net": 0,
+        "fee": 0,
+        "unit": 100,
+        "description": "Charge for order " + event.data.object.metadata.order_id,
+        "sender": event.data.object.metadata.customer,
+        "receiver": event.data.object.metadata.winery,
+    }
+    serializer = TransactionSerializer(data=data)
+    serializer.is_valid(raise_exception=True)
     txn = stripe_transaction(event.data.object.balance_transaction)
-    print(txn)
-    # serializer = TransactionSerializer(data=data)
-    # serializer.is_valid(raise_exception=True)
-    # serializer.save()
+    serializer.save(net=txn.net, fee=txn.fee)
 
 
 class StipeWebhookAPI(APIView):
