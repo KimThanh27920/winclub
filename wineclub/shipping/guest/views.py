6dc3859c10e123aa90e3d7c0610c1c515aaf81df
huudangnguyen <nguyenhuudang0206@gmail.com>
2022-10-03 09:24:57 +0700

    Update ShippingUnit Admin&Guest

--- wineclub/shipping/guest/views.py
+++ wineclub/shipping/guest/views.py
@@ -1,6 +1,4 @@
-from rest_framework_simplejwt import authentication
-from rest_framework import viewsets, generics, status, permissions
-from rest_framework.response import Response
+from rest_framework import viewsets, generics
 from .serializers import ShippingUnitSerializer
 from shipping.models import ShippingUnit
 
